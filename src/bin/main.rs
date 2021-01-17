496d0d804b985212bf5e2f2bcd2adc2319d1b8be
Kanari <iovo7c@gmail.com>
2021-01-17 14:55:22 +0800

    :sparkles: special judge

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,8 @@
 use clap::Clap;
-use iron_tank::{error::{Error, Result}, judge::{Judge, NormalJudge}};
+use iron_tank::{compare::{self, CompareMode}, judge::SpecialJudge};
 use iron_tank::{
-    compare::{self, CompareMode},
+    error::{Error, Result},
+    judge::{Judge, NormalJudge},
 };
 use std::{fs, path::Path};
 #[derive(Clap)]
@@ -20,6 +21,8 @@ struct Opts {
 enum SubCommand {
     #[clap(version = "0.1.0", about = "Judge in normal mode")]
     Normal(NormalJudgeConfig),
+    #[clap(version="0.1.0",about="Judge in special mode")]
+    Special(SpecialJudgeConfig),
     #[clap(version = "0.1.0", about = "Debug mode")]
     Debug,
 }
@@ -45,6 +48,20 @@ struct NormalJudgeConfig {
 }
 
 #[derive(Clap, Debug)]
+struct SpecialJudgeConfig {
+    #[clap(about = "path of program to run")]
+    exec: String,
+    #[clap(short, about = "input file path")]
+    input_file: String,
+    #[clap(short, default_value = "1024", about = "memory limit(MB)")]
+    memory_limit: u64,
+    #[clap(short, default_value = "30000", about = "time limit(MS)")]
+    time_limit: u64,
+    #[clap(short, about = "checker program path")]
+    checker: String,
+}
+
+#[derive(Clap, Debug)]
 struct DebugJudge {}
 
 fn main() -> Result<()> {
@@ -64,22 +81,52 @@ fn main() -> Result<()> {
                 .into());
             }
 
-            let input=fs::read_to_string(input_file_path)?;
-            let answer=fs::read_to_string(answer_file_path)?;
+            let input = fs::read_to_string(input_file_path)?;
+            let answer = fs::read_to_string(answer_file_path)?;
 
-            let comparation:Box<dyn CompareMode>=match config.compare_method.as_str() {
-                "full"=>Box::new(compare::GlobalCompare{}),
-                "line"=>Box::new(compare::LineCompare{}),
-                "value"=>Box::new(compare::ValueCompare{}),
-                _=>{
-                    Err(Error::Argument("comparation mode not found".into()))?
-                }
+            let comparation: Box<dyn CompareMode> = match config.compare_method.as_str() {
+                "full" => Box::new(compare::GlobalCompare {}),
+                "line" => Box::new(compare::LineCompare {}),
+                "value" => Box::new(compare::ValueCompare {}),
+                _ => Err(Error::Argument("comparation mode not found".into()))?,
             };
 
-            let judge=NormalJudge::new(config.exec, input, answer, config.memory_limit, config.time_limit, comparation);
-            let judge_result=judge.judge()?;
+            let judge = NormalJudge::new(
+                config.exec,
+                input,
+                answer,
+                config.memory_limit,
+                config.time_limit,
+                comparation,
+            );
+            let judge_result = judge.judge()?;
             println!("{:#?}", judge_result);
         }
+        SubCommand::Special(config) => {
+            let path = Path::new(&config.exec);
+            let input_file_path = Path::new(&config.input_file);
+            let checker_path = Path::new(&config.checker);
+
+            if !path.exists() || !input_file_path.exists() || !checker_path.exists() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::NotFound,
+                    "code, input or checker not found",
+                )
+                .into());
+            }
+
+            let input = fs::read_to_string(input_file_path)?;
+
+            let judge=SpecialJudge::new(
+                config.exec,
+                input,
+                config.memory_limit,
+                config.time_limit,
+                config.checker
+            );
+            let judge_result=judge.judge()?;
+            println!("{:#?}",judge_result);
+        }        
         SubCommand::Debug => {}
     }
 
--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,23 @@
+use std::string;
+
 pub type Result<T>=std::result::Result<T,Error>;
 
 #[derive(Debug)]
 pub enum Error{
     IO(std::io::Error),
     Argument(String),
+    Checker(String),
+    FromUtf8(string::FromUtf8Error),
 }
 
 impl From<std::io::Error> for Error{
     fn from(err: std::io::Error) -> Self {
         Self::IO(err)
+    }
+}
+
+impl From<string::FromUtf8Error> for Error{
+    fn from(err: string::FromUtf8Error) -> Self {
+        Self::FromUtf8(err)
     }
 }
