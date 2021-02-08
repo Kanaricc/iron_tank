6643ba49ca32dbda76cbb3681dae4e83bbc92db0
Kanari <iovo7c@gmail.com>
2021-02-08 10:52:25 +0800

    feat: init actix-web

--- tank_client/src/main.rs
+++ tank_client/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -10,15 +11,19 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-fn main() {
-    actix_rt::System::new("qwq").block_on(async move {
-        HttpServer::new(|| {
-            App::new()
-                .service(hello)
-                .service(echo)
-        })
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
-    }).unwrap();
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
