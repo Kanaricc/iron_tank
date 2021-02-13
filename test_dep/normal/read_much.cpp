766af2eebcbc2109f5edfe35c8fbb5341f95a3dc
Kanari <iovo7c@gmail.com>
2021-02-13 11:47:52 +0800

    tests: user try reading more inputs than expected

---
+++ test_dep/normal/read_much.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+vector<int> vec;
+int main(){
+    ios::sync_with_stdio(false);
+    // ofstream fout("out.txt");
+    // fout<<"???"<<endl;
+
+    vector<int> vec;
+    for(int i=0;i<10;i++){
+        vec.push_back(i*i);
+    }
+
+    int a;cin>>a;
+    cin>>a;
+    cout<<vec[0]+a*2<<endl;
+
+
+
+    return 0;
+}
